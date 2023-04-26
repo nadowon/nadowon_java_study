649e37e2c9e26370b67696f3abe9c74e61f158a3
nadowon <117194507+nadowon@users.noreply.github.com>
2023-04-26 16:13:14 +0900

    Update 라이브러리_String_4.java
    *회문 문제*
    Line 27 
    문자열의 맨 앞자리와 맨 끝자리를 비교할 때 for문과 if문을 사용한다
    default 값을 true로 초기화하고, if문 조건을 성립할 때를 false로 잡아놔야 효율적인 수행을 할 수 있다

--- Chapter9_라이브러리_2/src/com/sist/string/라이브러리_String_4.java
+++ Chapter9_라이브러리_2/src/com/sist/string/라이브러리_String_4.java
@@ -23,6 +23,15 @@ public class 라이브러리_String_4 {
 			break;
 		}
 		System.out.println("data="+data);
+		
+		boolean bCheck=true;
+		for(int i=0;i<data.length()/2;i++) {
+			if(data.charAt(i)!=data.charAt(data.length()-1-i)) {
+				bCheck=false;
+				break;
+			}
+		}
+		
 //		boolean bCheck=false;
 //		for(int i=0;i<data.length()/2;i++) {
 //			int j=data.length()-1;
@@ -34,17 +43,18 @@ public class 라이브러리_String_4 {
 //			}
 //			j--;
 //		}
-		boolean bCheck=false;
-		for(int i=0;i<data.length()/2;i++) {
-			char s=data.charAt(i);
-			char e=data.charAt(data.length()-1-i);
-			if(s==e)
-				bCheck=true;
-			else {
-				bCheck=false;
-				break;
-			}
-		}
+		
+// 		boolean bCheck=false;
+// 		for(int i=0;i<data.length()/2;i++) {
+// 			char s=data.charAt(i);
+// 			char e=data.charAt(data.length()-1-i);
+// 			if(s==e)
+// 				bCheck=true;
+// 			else {
+// 				bCheck=false;
+// 				break;
+// 			}
+// 		}
 		if(bCheck==true) {
 			System.out.println(data+"는 좌우대칭입니다");
 		}
